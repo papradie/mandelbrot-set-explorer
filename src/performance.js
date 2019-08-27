59848ec4e2296698188c3d4e522ae7fa68fd1026
papradie <pavol.jurov@gmail.com>
2019-08-27 11:35:41 +0200

    Add formatting console output of the performance log messages

--- src/performance.js
+++ src/performance.js
@@ -3,7 +3,7 @@ const withPerf = (fn, args) => {
     const t0 = performance.now();
     const result = fn.apply(null, args);
     const t1 = performance.now();
-    console.log(`Function ${fn.name} run in ${Math.floor(t1 - t0)} milliseconds`);
+    console.log(`%cFunction ${fn.name} run in ${Math.floor(t1 - t0)} milliseconds`, 'color: blue');
     return result;
 }
 
