bb97a1bea366d43b99c99cd6feb2145e929f2732
papradie <pavol.jurov@gmail.com>
2019-08-27 11:29:07 +0200

    Add formatting console output of the statistics data

--- src/statistics.js
+++ src/statistics.js
@@ -1,13 +1,42 @@
-import { maxIterations } from './constants'
+import { canvasProps, maxIterations } from './constants'
 
 const pointsStats = points => {
     const flatPoints = points.flat();
 
-    let sum = flatPoints.reduce((acc, curr) => acc + curr, 0);
-    console.log(`Average iterations count is ${sum / (600 * 400)}`);
+    let data = [
+        totalIterations(flatPoints),
+        averageIterations(flatPoints),
+        maxIterationsReached(flatPoints),
+        maxIterationsPercentage(flatPoints)
+    ];
+    printResults(data);
+}
 
-    let maxIterationsReached = flatPoints.filter(x => x === maxIterations).length;
-    console.log(`Max iterations reached in ${maxIterationsReached} cases (${maxIterationsReached / flatPoints.length * 100}%)`)
+const totalIterations = flatPoints => ({
+    desc: 'Total iterations count  ',
+    value: flatPoints.reduce((acc, curr) => acc + curr, 0)
+});
+
+const averageIterations = flatPoints => ({
+    desc: 'Average iterations count',
+    value: Math.floor(totalIterations(flatPoints).value / (canvasProps.width * canvasProps.height))
+});
+
+const maxIterationsReached = flatPoints => ({
+    desc: 'Max iterations reached  ',
+    value: flatPoints.filter(x => x === maxIterations).length
+});
+
+const maxIterationsPercentage = flatPoints => ({
+    desc: 'Max iterations percents ',
+    value: `${Math.floor(maxIterationsReached(flatPoints).value / flatPoints.length * 100)}%`
+});
+
+const printResults = data => {
+    const cssStyle = 'color: orange'
+    console.log("%c=== STATISTICS ==========================", cssStyle);
+    data.forEach(item => console.log(`%c=== ${item.desc}\t\t\t${item.value}`, cssStyle));
+    console.log("%c=========================================", cssStyle);
 }
 
 export { pointsStats }
